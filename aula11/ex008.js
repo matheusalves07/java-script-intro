cd4876519ea312c569757d36908e247579071330
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2022-12-08 05:34:11 -0300

    js dom

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 62.0
+console.log(`A velocidade do seu carro é ${vel} kilometros por hora`)
+if(vel >60){
+    console.log('Você ultaprassou a velocidade permitida ! Multado !')
+}
+console.log('Dirija sempre usando cinto de segurança')
