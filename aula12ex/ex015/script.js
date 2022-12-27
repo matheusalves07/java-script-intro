feab9625e76a7f7f4a1b221bbccbde602f8c6619
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2022-12-26 23:54:38 -0300

    verificador - corrigir
    corrigir erros de if

--- aula12ex/ex015/script.js
+++ aula12ex/ex015/script.js
@@ -1,1 +1,55 @@
-alert('Olá!')
+function verificar(){
+var data = new Date()
+var ano = data.getFullYear()
+var fano = document.getElementById('txtano')
+var res = document.querySelector('div#res')
+    if(fano.value.lengh == 0 || Number(fano.value) > ano){
+    window.alert('Verifique os dados e tente   novamente')
+    } else {
+      var fsex = document.getElementsByName('radsex')
+      var idade = ano - Number(fano.value)
+      var genero = ""
+      var img = document.createElement('img')
+      img.setAttribute('id','foto')
+      if (fsex [0].checked) {
+        genero = 'masculino'
+
+        if (idade =>0 && idade <10){
+            //criança
+            img.setAttribute('src','crainca-menino.png')
+
+        } else if(idade <21) {
+            //jovem
+            img.setAttribute('src','adolescente-menino.png')      
+        }else if (idade <50){
+            //adulto
+            img.setAttribute('src', 'adulto-homen.png')
+        }else  {  
+            //idoso
+            img.setAttribute('src,','idoso-homen.png')
+        } 
+      }else if (fsex[1].checked) {
+        genero = 'feminino'
+        if (idade =>0 && idade <10){
+            //criança
+            img.setAttribute('src','crianca-menina.png')
+        }else if ( idade <21){
+            //jovem
+            img.setAttribute('src','adolescente-menina.png')    
+        }else if (idade <50){
+            //adulto
+            img.setAttribute('src','adulto-mulher.png')
+        }else {  
+            //idoso
+            img.setAttribute('src,','idoso-mulher.png')
+        }
+      } 
+      res.style.textAlign = 'center'
+      res.innerHTML = `Detectamos ${genero} com ${idade} anos`
+      res.appendChild(img)
+       
+      
+
+    }
+
+}
