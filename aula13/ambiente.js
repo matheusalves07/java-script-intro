52c4dd46e97a9d32f75e1d2d807ce575cd1dee1d
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2022-12-28 19:35:26 -0300

    backend

---
+++ aula13/ambiente.js
@@ -0,0 +1,24 @@
+var c= 1 
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+}
+
+/*
+var c= 1 
+while (c <= 6){
+    console.log(`Passo ${c}`)
+    c++
+} */
+
+
+
+
+/*
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+console.log('Tudo Bem')
+*/
+
