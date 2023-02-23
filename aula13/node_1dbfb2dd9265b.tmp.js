405d5b5c2ecd6f1ffc42b0d3f4bd15653f373298
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-02-23 10:37:59 -0300

    while

---
+++ aula13/node_1dbfb2dd9265b.tmp.js
@@ -0,0 +1,12 @@
+var c = 1
+
+do {
+    console.log(`Passo ${c}passo`)
+    c++
+
+
+} while(c =>450 && c <=500)
+
+
+//do =
+// while = 
---
+++ aula13/repeticao.js
@@ -0,0 +1,12 @@
+var c = 10
+
+do {
+    console.log(`Passo ${c}passo`)
+    c++
+
+
+} while ( c <=6)
+
+
+//do =
+// while = 
