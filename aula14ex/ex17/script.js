847346ff1270c05e97d28b456db134b254d2b40b
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-03-16 09:17:26 -0300

    Update script.js

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -7,6 +7,7 @@ function tabuada(){
         let n = Number(num.value)
         let c = 1
         tab.innerHTML = ''
+        
         while(c <= 20){
             let item = document.createElement('option')
             item.text =`${n} x ${c} = ${n* c}`
