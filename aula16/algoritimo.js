730d649a86e6df5764c1ebd28e8526f6e02aa964
Matheus alves <106258281+matheusalves07@users.noreply.github.com>
2023-04-26 07:45:55 -0300

    Update algoritimo.js

--- aula16/algoritimo.js
+++ aula16/algoritimo.js
@@ -1,6 +1,7 @@
 // variaveis e tipos de dado 
-var num1 = 1;
-var num2 = 1;
+var num1 = 4;
+var num2 = 2;
 var soma = num1+ num2 ;
+var divisao = num1/num2;
 
-console.log();
+console.log( divisao);
